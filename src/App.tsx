b116f9cc8c6c16beccaa88be30d41e003bf60909
SysSyncer <harikrishnan4665@gmail.com>
2025-05-23 12:36:20 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>This react <a href='https://vite.dev/' style={{color: 'orange'}}>vite</a> app is successfully deployed in <a href="https://tiiny.host/" style={{color: "violet"}}>tiiny.host</a></h1>
+      <code style={{fontSize: 20}}><a href="https://github.com/SysSyncer/Webpage-on-tiiny.git">/source-code</a></code>
+    </>
+  )
+}
+
+export default App
